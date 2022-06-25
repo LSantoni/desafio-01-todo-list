486e744094f11c7118fb64225d282a764541fc84
Lizandra <lizandra.silva@evosystems.com.br>
2022-06-25 09:54:55 -0300

    finalizar Layout de NewTask

--- src/components/NewTask.tsx
+++ src/components/NewTask.tsx
@@ -1,11 +1,13 @@
+import { PlusCircle } from 'phosphor-react';
 import styles from './NewTask.module.css';
 
 export function NewTask() {
   return (
-    <div className="container">
-      <input type="text" />
+    <div className={styles.container}>
+      <input type="text" placeholder='Adicione uma nova tarefa' />
       <button>
         Criar
+        <PlusCircle size={16} />
       </button>
     </div>
   )
