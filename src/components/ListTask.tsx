250c615f3dd929df39ca209c018284e3af1e292b
Lizandra <lizandra.silva@evosystems.com.br>
2022-06-25 11:56:15 -0300

    Finalizar layout de Listar tarefas

--- src/components/ListTask.tsx
+++ src/components/ListTask.tsx
@@ -1,5 +1,27 @@
+import styles from './ListTask.module.css';
+
+import clipboard from '../assets/clipboard.svg';
+
 export function ListTask() {
   return (
-    <h1>Lista de Tasks</h1>
+    <div className={styles.container}>
+      <header>
+        <p>
+          Tarefas criadas
+          <span>0</span> 
+        </p>
+
+        <p>
+          Concluídas
+          <span>0</span>
+        </p>
+      </header>
+
+      <section>
+        <img src={clipboard} alt="icone de quando não tem nenhuma tarefa criada" />
+        <p>Você ainda não tem tarefas cadastradas</p>
+        <p>Crie tarefas e organize seus itens a fazer</p>
+      </section>
+    </div>
   )
 }
